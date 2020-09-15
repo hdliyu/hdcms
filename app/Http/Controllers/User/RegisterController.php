eb14c766b679c1d2b8840becf7847bb7831c130b
hdliyu <278746453@qq.com>
2020-09-15 16:48:34 +0800

    fix

--- app/Http/Controllers/User/RegisterController.php
+++ app/Http/Controllers/User/RegisterController.php
@@ -10,14 +10,15 @@ class RegisterController extends Controller
 {
     public function __invoke(Request $request,User $user)
     {
-        $post = $this->validate($request,[
-            'email'=>['sometimes','email'],
-            'name'=>['required','min:3','max:50'],
-            'password'=>['required','min:5','max:50','confirmed'],
+        $request->validate([
+            'email' => ['sometimes', 'email'],
+            'name' => ['required', 'min:3', 'max:50'],
+            'password' => ['required', 'min:5', 'max:50', 'confirmed']
         ]);
-        $user->name = $post['name'];
-        $user->email = $post['email'];
-        $user->password = $post['password'];
+        $user->password = $request->password;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
         return $user;
     }
 }
